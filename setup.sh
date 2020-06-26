4cedd57f651fa9d1c24a5ad059942359f8bf56f5
szkusisi <szk@usisi.net>
2020-06-27 08:26:06 +0900

    Default Message

---
+++ setup.sh
@@ -0,0 +1,20 @@
+# sudo user
+
+# update and upgrade
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+
+# install
+sudo apt install -y ssh vim git net-tools 
+# vim apt-utils vim git net-tools wget ssh curl
+# ethtool(WOL)
+
+# git init
+# git config --global credential.helper store
+# git config --global user.email "szk@usisi.net"
+# git config --global user.name "szkusisi"
+
+# tool
+# gitauto
+# sudo cp bin/gitauto /usr/local/bin/
