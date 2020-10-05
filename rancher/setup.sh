220bc61a4f308d7a91f21eaa0e5a5c361c6b7b17
szkusisi <szk@usisi.net>
2020-10-05 13:51:35 +0000

    Default Message

---
+++ rancher/setup.sh
@@ -0,0 +1,42 @@
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git curl
+
+sudo apt install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible -y
+
+
+curl https://releases.rancher.com/install-docker/19.03.sh | sh
+
+sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+ssh-keygen -b 4096
+ssh-copy-id root@localhost
+
+
+# Docker Install
+curl https://releases.rancher.com/install-docker/19.03.sh | sh
+
+# RKE Install
+curl -s https://api.github.com/repos/rancher/rke/releases/latest | grep download_url | grep amd64 | cut -d '"' -f 4 | wget -qi -
+chmod +x rke_linux-amd64
+sudo mv rke_linux-amd64 /usr/local/bin/rke
+rke --version
+
+# HELM Install
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+
+# kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
