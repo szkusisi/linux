b3c8978ce381be425791f9d1d850871a54c00ff5
szkusisi <szk@usisi.net>
2020-10-06 01:21:30 +0000

    Default Message

--- rancher/setup.sh
+++ rancher/setup.sh
@@ -23,17 +23,22 @@ ssh-copy-id root@localhost
 
 # Docker Install
 curl https://releases.rancher.com/install-docker/19.03.sh | sh
+sudo usermod -aG docker $USER
+
 
 # RKE Install
 curl -s https://api.github.com/repos/rancher/rke/releases/latest | grep download_url | grep amd64 | cut -d '"' -f 4 | wget -qi -
 chmod +x rke_linux-amd64
 sudo mv rke_linux-amd64 /usr/local/bin/rke
 rke --version
+rm -rf rke_darwin-amd64  rke_windows-amd64.exe
+
 
 # HELM Install
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+rm -rf  get_helm.sh
 
 
 # kubectl
